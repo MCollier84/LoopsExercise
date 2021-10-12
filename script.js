8a60e60eee8b76e64a3667f91e90e723e52cbd79
Maurice Collier <mauricecollier84@yahoo.com>
2021-10-11 22:44:53 -0400

    Created LoopsExercise script.js file without the bonus.

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1
+for (i = 1; i < 8; i++){
+    console.log(i);
+}
+
+// 2
+console.log("//////");
+
+for (i = 5; i <= 25; i += 4){
+    console.log(i);
+}
+
+// 3a
+const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+]
+
+// 3b
+for (hpchr of wizards){
+    console.log(hpchr);
+}
+
+// 4a
+let harryPotterMovies = 0;
+console.log(harryPotterMovies);
+
+// 4b
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
+
+// 4c
+console.log(harryPotterMovies = 8);
